89beb717534260b0d67da1d545ff570734683646
Your Name <you@example.com>
2019-10-04 00:06:54 +0530

    success build

--- FreeCrmBddFramework/src/main/java/Runner/TestRunner.java
+++ FreeCrmBddFramework/src/main/java/Runner/TestRunner.java
@@ -6,7 +6,11 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "Feature", glue = { "stepDefinition" } )// format = { "pretty", "html:test-output" })
+@CucumberOptions(features = "F:/Git_repo/CRM_BDD_FW_V1.0/FreeCrmBddFramework/src/main/java/Features/Login.feature", glue = { "stepDefinition" } )// format = { "pretty", "html:test-output" })
 public class TestRunner {
 
 }
+
+/*
+ * Feature file pathi should be right
+ */
