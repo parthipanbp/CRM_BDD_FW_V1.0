898f7188baa639390be675aa4c6ca0636b7ffbe3
Your Name <you@example.com>
2019-10-03 23:52:10 +0530

    first result

--- FreeCrmBddFramework/src/main/java/Features/Login.feature
+++ FreeCrmBddFramework/src/main/java/Features/Login.feature
@@ -2,7 +2,7 @@ Feature: Free CRM login Feature
 
 Scenario: Launch and Login CRM Application 
 Given user is already on Login Page 
-When litile of login pge is Free CRM
+When title of login page is Free CRM
 And user enters user and password
  
 
--- FreeCrmBddFramework/src/main/java/stepDefinitions/LoginStepDefinition.java
+++ FreeCrmBddFramework/src/main/java/stepDefinitions/LoginStepDefinition.java
@@ -1,12 +1,21 @@
 package stepDefinitions;
 
+/*
 import cucumber.api.java.en.Given;
-
+import cucumber.api.java.en.When;
+*/
 public class LoginStepDefinition {
 	
-	@Given("^user is already on Login Page$")
-	public void userisalreadyonloginpage() {
-		
-	}
+	/*
+	 * @Given("^user is already on Login Page$") public void
+	 * userisalreadyonloginpage() {
+	 * 
+	 * }
+	 * 
+	 * @When("^title of login page is Free CRM$") public void
+	 * titleofloginpageisFreeCRM() {
+	 * 
+	 * }
+	 */
 
 }
