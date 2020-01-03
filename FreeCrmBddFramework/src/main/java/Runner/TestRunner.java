69a9442075e273949fc73a8d77140cf995c548f7
Parthipan Balakrishnan <parthipanbp@gmail.com>
2020-01-04 00:13:06 +0530

    added differnet output format like HTML ,Json,XMl report files html:Test_output,json:json_output_file/report.json,junit:xml_output_file/report.xml
    and added Strict option,its used to strictly ensure the implementation
    of stepdefinition of the featrue setp ,if not thorws an error to define
    it

--- FreeCrmBddFramework/src/main/java/Runner/TestRunner.java
+++ FreeCrmBddFramework/src/main/java/Runner/TestRunner.java
@@ -8,8 +8,9 @@ import cucumber.api.junit.Cucumber;
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "F:/Git_repo/CRM_BDD_FW_V1.0/FreeCrmBddFramework/src/main/java/Features/Login.feature", tags="@test",//"feature"->a path of feature file 
 glue = { "stepDefinitions" }, // "Glue"->a path of stepdefiniton file 
-format = { "pretty", "html:test-output" },
-dryRun = false,//"dryRun" -> verify the mapping between feature and Stepdefinition steps 
+format = { "pretty", "html:test-output", "json:Json_output_file/Output.json", "junit:xml_output_file/Output.xml" },//to generate differnet type of report like HTML,Json,XML 
+dryRun = false,//"dryRun" -> verify the mapping between feature and Stepdefinition steps
+strict = true, // To strickly ensur the stepdefiniton for the feature steps ,if step is not defined its thorws execetion immediatly 
 monochrome=true) // Produce the proper readble consol output(True/False)
 
 public class TestRunner {
